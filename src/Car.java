ce1965d3e009c485b4d3d4c4dbf305451acec68d
David Mesquita <dmesquita5678@gmail.com>
2025-01-29 13:07:15 -0500

    completed assignment

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,22 @@ public class Car extends Vehicle {
 
     private String brand;
 
+    public Car(Vehicle v1, String brand){
+        super(v1.getNumberOfWheels(), v1.getColor(), v1.getEngineSize(), v1.getFuelType());
+        this.brand=brand;
+    }
+
+    public void honk(){
+        System.out.println("honk honk");
+    }
+
+    public void displayInfo(){
+        System.out.println("Number of Wheels: " + super.getNumberOfWheels());
+        System.out.println("Color: " + super.getColor());
+        System.out.println("Engine Size: " + super.getEngineSize());
+        System.out.println("Fuel Type: " + super.getFuelType());
+        System.out.println("Brand: " + this.brand);
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vehicle v1 = new Vehicle(4, "yellow", 7, "diesel");
+        Car c1 = new Car(v1, "Volkswagen");
+        c1.honk();
+        c1.displayInfo();
     }
 }
